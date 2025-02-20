be34ac181d5f1d85c5b18dde187a41a6e03d5d80
pamul1 <109163253+pamul1@users.noreply.github.com>
2025-02-19 18:57:34 -0600

    Commit3

--- Controllers/earningsController.js
+++ Controllers/earningsController.js
@@ -2,14 +2,25 @@ import { db } from "../cn.js"
 
 export const getEarning = async (req, res) => {
 
-    const sql = 'select * from earnings'
+    const sql = `select a.earnings_id, 
+                        a.movie_id, 
+                        a.country, 
+                        '$'||trim(to_char(a.revenue, '999,999,999')) revenue, 
+                        b.title as movie_name
+                        from earnings a
+                        inner join movie b 
+                        on a.movie_id = b.movie_id`;
+    try {
     const result = await db.query(sql)
     res.status(200).json(result)
+    }catch(err){
+        res.status(200).json(err.message)
+    }
 
 }
 
 export const postEarning = async (req, res) => {
-   
+
     const tmp = req.body
 
     if (!tmp.movie_id) {
@@ -29,12 +40,12 @@ export const postEarning = async (req, res) => {
 
     try {
         const str = 'insert into earnings ( movie_id, country, revenue) values ($1, $2, $3)'
-        const arr = [ tmp.movie_id, tmp.country, tmp.revenue]
+        const arr = [tmp.movie_id, tmp.country, tmp.revenue]
         const result = await db.query(str, arr)
         res.status(200).json({ message: "Earning Added" })
         return
     } catch (err) {
-        res.status(500).json({message: err.message})
+        res.status(500).json({ message: err.message })
         return
     }
 
@@ -80,19 +91,19 @@ export const deleteEarning = async (req, res) => {
 
     const earning_id = req.params.id
 
-    if (!earning_id){
-        res.status(300).json({message: "Field earning_id is empty"})
+    if (!earning_id) {
+        res.status(300).json({ message: "Field earning_id is empty" })
         return
     }
 
-    try{
+    try {
         const str = 'delete from earnings (earning_id) values ($1)'
         const arr = [earning_id]
         const result = await db.query(str, arr)
         res.status(200).json({ message: "Earning Deleted" })
         return
-    } catch(err){
-        res.status(500).json({message:err.message})
+    } catch (err) {
+        res.status(500).json({ message: err.message })
         return
     }
 
